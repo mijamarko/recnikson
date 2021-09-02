bf0d515496ff45f66187ec409824fd886e52f175
Marko Mijajlović <marko.b.mijajlovic@gmail.com>
2021-09-02 12:37:49 +0200

    minor changes

--- src/components/WordMeaning/WordMeaning.js
+++ src/components/WordMeaning/WordMeaning.js
@@ -15,7 +15,8 @@ const WordMeaning = (props) => {
   return (
     <div className="wordMeaning">
       <div className="meaningSelector">
-        {wordMeanings && wordMeanings.map((e, idx) => <a href="/#" index={idx} onClick={changeMeaning} key={e.partOfSpeech}>{e.partOfSpeech.slice(0, 1).toUpperCase()}{e.partOfSpeech.slice(1)}</a>)}
+        {wordMeanings && wordMeanings.map((e, idx) => <a href="/#" className={'meaning'} index={idx} onClick={changeMeaning} key={e.partOfSpeech}>
+          {e.partOfSpeech.slice(0, 1).toUpperCase()}{e.partOfSpeech.slice(1)}</a>)}
       </div>
       <div className="wordDefinitions">
         {wordMeanings[meaningIndex].definitions && wordMeanings[meaningIndex].definitions.map((e, idx) => <DefinitionCard definitionData={e} key={idx}/>)}
